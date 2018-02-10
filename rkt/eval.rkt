4a30542e71358696ef4a71bb8733e13590c44359
Todd V. Jonker <todd@toddjonker.net>
2018-02-10 13:55:53 -0800

    WIP Update NSI test in Racket.
    Looks like the behavior changed <= 5.3
    
    Issue ID FUSION-117

--- rkt/eval.rkt
+++ rkt/eval.rkt
@@ -1,18 +1,29 @@
-;; Copyright (c) 2014 Amazon.com, Inc.  All rights reserved.
+;; Copyright (c) 2014-2018 Amazon.com, Inc.  All rights reserved.
 
 (require rackunit)
 
 
 (define ns (make-base-namespace))
 
-(eval '(define top 17220) ns)
+(eval '(define top "other-ns") ns)
 
-(define top 17110)
-
-(check-eq?
-  (eval (quote-syntax (+ top 2)) ns)
-  17222)
+(define top "this-ns")
 
 (check-eq?
-  (eval (list (quote +) (quote-syntax top) 2) ns)
-  17222)
+  (eval (quote top) ns)
+  "other-ns")
+
+; Fails in Racket 5.3, 6.5:
+;   require: namespace mismatch; reference to a module that is not available
+;
+; This seems to contradict the documentation for namespace-syntax-introduce,
+; which states:
+;
+;   The additional context is overridden by any existing top-level bindings in
+;   the syntax object’s lexical information
+;
+; Here `top` has an existing top-level binding, yet its not being used.
+
+(check-eq?
+  (eval (quote-syntax top) ns)
+  "other-ns")
