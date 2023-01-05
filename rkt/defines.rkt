97f5a6892f7a784854c936b6a146f99b746ce9d0
Todd V. Jonker <todd@toddjonker.net>
2023-01-04 17:30:25 -0800

    WIP Does Racket not work as documented?

--- rkt/defines.rkt
+++ rkt/defines.rkt
@@ -1,7 +1,18 @@
 #lang racket
 
-(module M racket
+(module M1 racket
   (require "macro_defines.rkt")
+  (define multiple 1)
   (defpub_macro_introduced_name multiple 1658))
 
-(require 'M)
+(module M2 racket
+  (define multiple 2)
+  (provide multiple))
+
+
+(require 'M1)
+;(require 'M2)
+
+(define sum (+ multiple 1))
+
+(define multiple 10)  ;; This is surprising and seems to contradict Racket docs.
